1a6e08acf6250880076d6e56f82ce2763f3d0c25
Odilio Witteveen <git@odil.io>
2022-04-20 13:54:38 +0200

    Update plugin description
    Signed-off-by: Odilio Witteveen <git@odil.io>

--- wp-github-activity.php
+++ wp-github-activity.php
@@ -8,13 +8,13 @@
  * @license     GPL-3.0-or-later
  *
  * @wordpress-plugin
- * Plugin Name: Plugin Name
+ * Plugin Name: Github Activity
  * Plugin URI:  https://odil.io/wordpress/wp-github-activity
- * Description: Description of the plugin.
+ * Description: Display your Github Activity.
  * Version:     1.0.0
  * Author:      Odilio Witteveen
  * Author URI:  https://odil.io
- * Text Domain: plugin-slug
+ * Text Domain: wp-github-activity
  * License:     GPL v3 or later
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  */
